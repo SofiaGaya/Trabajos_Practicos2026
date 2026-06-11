c934e6551627935e1328a9a7e52e500a21ae73f2
alumno <alumno@Instituto.local>
2026-06-11 08:22:21 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -25,12 +25,12 @@ divicion = numero1 / numero2;
 console.log(" el resultado de dividir " + numero1 + " / " + numero2 + " es " + divicion);
 alert(" el resultado de dividir " + numero1 + " / " + numero2 + " es " + divicion)
 
-let nombreAlumno = " luis "
-let apellidoAlumno = " gimenez "
+let nombreAlumno = " Sofia "
+let apellidoAlumno = " Gaya "
 let edadAlumno = 14
 
-console.log(" el alumno : " + nombreAlumno + apellidoAlumno + " tiene " + edadAlumno + " años ");
-alert(" el alumno : " + nombreAlumno + apellidoAlumno + " tiene " + edadAlumno + " años ");
+console.log(" la alumna : " + nombreAlumno + apellidoAlumno + " tiene " + edadAlumno + " años ");
+alert(" la alumna : " + nombreAlumno + apellidoAlumno + " tiene " + edadAlumno + " años ");
 
 let precio1 = 250
 let precio2 = 250
@@ -39,3 +39,21 @@ let total = precio1 + precio2 + precio3
 
 console.log( " el total a pagar es : " + total)
 alert( " el total a pagar es : " + total)
+
+let comidaFavorito = " pastel de papa"
+let colorFavorito = " azul "
+let numeroFavorito = 9
+
+console.log( " mi comida favorita es el " + comidaFavorito + ", me gusta el color " + colorFavorito + " y mi numero favorito es " + numeroFavorito) 
+alert( " mi comida favorita es el " + comidaFavorito + ", me gusta el color " + colorFavorito + " y mi numero favorito es " + numeroFavorito)
+
+let pais = " Argentina "
+
+alert( pais )
+console.log( pais )
+
+pais = " Brasil "
+
+alert( pais )
+console.log( pais )
+
