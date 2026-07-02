9b55a82266a0e7c9aa255d1aa63d8163eba8a507
alumno <alumno@Instituto.local>
2026-07-02 09:15:17 -0300

    2/7/26

---
+++ TP6/app.js
@@ -0,0 +1,17 @@
+let body = document.querySelector ('body')
+let titulo = document.querySelector('h1')
+let parrafo = document.querySelector("p")
+let fondo = document.querySelector("#b1")
+let texto = document.querySelector("#b2")
+let color = document.querySelector("#b3")
+let CambiarTexto = document.querySelector("#b4")
+
+fondo.onclick = function(){
+ body.style.backgroundColor = 'rgb(230, 235, 164)'
+}
+texto.onclick = function(){
+    parrafo.textContent = " hola como estan "
+}
+color.onclick = function(){
+    parrafo.color = "orange"
+}
