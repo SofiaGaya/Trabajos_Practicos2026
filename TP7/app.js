ab0207ba80d119cf7e37500680fb1c638cc58728
alumno <alumno@Instituto.local>
2026-08-06 08:51:09 -0300

    06/08

--- TP7/app.js
+++ TP7/app.js
@@ -1,5 +1,22 @@
-let edad = 16
-if (edad >= 18)
-alert( "bienvenido" )
-else
-alert( " no permitido ingresar ")
+let edad = 18
+let ejercicio1 = document.querySelector ('#parrafo1')
+let ejecutar1 = document.querySelector ("#boton1")
+
+ejecutar1.onclick = function(){
+    if (edad >= 18) {
+        ejercicio1.textContent = " Podes ingresar "
+    } else {
+        ejercicio1.textContent = " No podes ingresar "
+    }
+}
+let NombreUsuario = "Nahuel"
+let ejercicio2 = document.querySelector ("#parrafo2")
+let ejecutar2 = document.querySelector ("#boton2")
+
+ejecutar2.onclick = function(){
+    if (NombreUsuario == "Nahuel") {
+        ejercicio2.textContent = "Bienvenido Nahuel"
+    } else {
+        ejercicio2.textContent = "Bienvenido Usuario"
+    }
+}
